c610ed2e6733bce62dc46d4620e0f047e4e235d7
mhdfaisal <fmohd195@gmail.com>
2021-04-16 23:24:55 +0530

    add eslint, prettier for linting and code formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const App: React.FC = () => {
-	return <div className='App'>My Digital Resume</div>;
-};
+const App: React.FC = () => <div className="App">My Digital Resume</div>;
 
 export default App;
