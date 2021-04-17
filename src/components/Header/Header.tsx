8f595170b16db15ec0f699b106fe984da7758025
mhdfaisal <fmohd195@gmail.com>
2021-04-17 21:34:34 +0530

    add a header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import NavBar from '../NavBar/NavBar';
+
+/**
+ * Header component to encapsulate top nav bar.
+ */
+const Header: React.FC = () => (
+  <header>
+    <NavBar />
+  </header>
+);
+
+export default Header;
