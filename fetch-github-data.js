d5bf4d7e691fac39db9809b00d13d71bd2b9e4f5
mhdfaisal <fmohd195@gmail.com>
2021-04-19 01:50:16 +0530

    add a function to fetch github profile data and write it to a file in the public directory

---
+++ fetch-github-data.js
@@ -0,0 +1,107 @@
+const fs = require('fs');
+const axios = require('axios');
+require('dotenv').config();
+
+/**
+ * An IIFE that queries the Github GraphQL API using the axios client
+ * and writes the data received from the API in a file at location
+ * public/github-profile.json inside the current project directory.
+ *
+ * @throws {Error} If the GITHUB_USERNAME or GITHUB_USERNAME is undefined or the
+ * network request fails or any error while writing to the file system.
+ */
+(() => {
+  // credentials from env file
+  const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+  const { GITHUB_USERNAME, GITHUB_GRAPHQL_API_URL } = process.env;
+
+  if (GITHUB_USERNAME === undefined) {
+    throw new Error('GITHUB_USERNAME env variable not found');
+  }
+
+  if (GITHUB_GRAPHQL_API_URL === undefined) {
+    throw new Error('GITHUB_GRAPHQL_API_URL env variable not found');
+  }
+  console.log(`Fetching github profile for ${GITHUB_USERNAME}`);
+
+  // graphql query
+  const data = JSON.stringify({
+    query: `
+{
+  user(login:"${GITHUB_USERNAME}") { 
+    name
+    bio
+    url
+    isHireable
+    avatarUrl
+    location
+    status {
+        emojiHTML
+        message
+      }
+    company
+    pinnedItems(first: 6, types: [REPOSITORY]) {
+      totalCount
+      edges {
+          node {
+            ... on Repository {
+              name
+              description
+              forkCount
+              stargazers {
+                totalCount
+              }
+              homepageUrl
+              pushedAt
+              openGraphImageUrl
+              usesCustomOpenGraphImage
+              repositoryTopics(first:6){
+                nodes{
+                  topic{
+                    name
+                  }
+                }
+              }
+              url
+              id
+              diskUsage
+              primaryLanguage {
+                name
+                color
+              }
+            }
+          }
+        }
+      }
+    }
+}
+`,
+  });
+
+  // making a network request using axios client
+  axios({
+    url: GITHUB_GRAPHQL_API_URL,
+    headers: {
+      Authorization: `Bearer ${GITHUB_TOKEN}`,
+      'User-Agent': 'Node',
+    },
+    method: 'post',
+    data,
+  })
+    .then((result) => {
+      // writing the API response to a file.
+      fs.writeFile(
+        './public/github-profile.json',
+        JSON.stringify(result.data),
+        (err) => {
+          // throw an error
+          if (err) return console.log(err);
+          console.log('saved file to public/github-profile.json');
+        }
+      );
+    })
+    .catch((err) => {
+      // throw an error
+      throw err;
+    });
+})();
