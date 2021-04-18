0c10a01b4e934a5e6dd6d79d44415b86c307fa42
mhdfaisal <fmohd195@gmail.com>
2021-04-18 20:48:57 +0530

    enclose EducationInfo and WorkExperience in an accordion

--- src/components/EducationInfo/EducationInfo.tsx
+++ src/components/EducationInfo/EducationInfo.tsx
@@ -1,39 +1,59 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import emoji from 'react-easy-emoji';
+
 import InfoCard from '../shared/InfoCard/InfoCard';
 
 import { educationInfo as educationInfoData } from '../../assets/portfolio-info';
+import './EducationInfo.css';
 
 /**
  * A presentational component to display educational information.
  */
 const EducationInfo: React.FC = () => (
   <Grid classes={{ root: 'education__info' }}>
-    <Typography variant="h6" gutterBottom color="textSecondary">
-      Educational Background
-    </Typography>
-    {educationInfoData &&
-      educationInfoData.map((educationItem) => {
-        const {
-          collegeName,
-          collegeLogo,
-          duration,
-          description,
-          descriptionBullets,
-        } = educationItem;
-        return (
-          <Grid item xs={12} key={Math.random()}>
-            <InfoCard
-              title={collegeName}
-              metaData={duration}
-              description={description}
-              descriptionBullets={descriptionBullets}
-              imageURL={collegeLogo}
-            />
-          </Grid>
-        );
-      })}
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="education__info-content"
+        id="education__info-header"
+      >
+        <Typography variant="h6" gutterBottom color="textSecondary">
+          <span className="education__info-header-icon">{emoji('🎓')}</span>{' '}
+          <span> Education</span>
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Grid container>
+          {educationInfoData &&
+            educationInfoData.map((educationItem) => {
+              const {
+                collegeName,
+                collegeLogo,
+                duration,
+                description,
+                descriptionBullets,
+              } = educationItem;
+              return (
+                <Grid item xs={12} key={Math.random()}>
+                  <InfoCard
+                    title={collegeName}
+                    metaData={duration}
+                    description={description}
+                    descriptionBullets={descriptionBullets}
+                    imageURL={collegeLogo}
+                  />
+                </Grid>
+              );
+            })}
+        </Grid>
+      </AccordionDetails>
+    </Accordion>
   </Grid>
 );
 
--- src/components/WorkExperience/WorkExperience.tsx
+++ src/components/WorkExperience/WorkExperience.tsx
@@ -1,41 +1,61 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import emoji from 'react-easy-emoji';
+
 import InfoCard from '../shared/InfoCard/InfoCard';
 
 import { workExperiences as workExperiencesData } from '../../assets/portfolio-info';
+import './WorkExperience.css';
 
 /**
  * A presentational component to display work experience information.
  */
 const WorkExperience: React.FC = () => (
-  <Grid classes={{ root: 'education__info' }}>
-    <Typography variant="h6" gutterBottom color="textSecondary">
-      Work Experience
-    </Typography>
-    {workExperiencesData &&
-      workExperiencesData.map((workExperienceItem) => {
-        const {
-          role,
-          companyName,
-          duration,
-          description,
-          descriptionBullets,
-          companyLogo,
-        } = workExperienceItem;
-        return (
-          <Grid item xs={12} key={Math.random()}>
-            <InfoCard
-              title={role}
-              subtitle={companyName}
-              metaData={duration}
-              description={description}
-              descriptionBullets={descriptionBullets}
-              imageURL={companyLogo}
-            />
-          </Grid>
-        );
-      })}
+  <Grid classes={{ root: 'work__experience' }}>
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="work__experience-content"
+        id="work__experience-header"
+      >
+        <Typography variant="h6" gutterBottom color="textSecondary">
+          <span className="work__experience-header-icon">{emoji(' 🏗️')}</span>
+          <span>Work Experience</span>
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Grid container>
+          {workExperiencesData &&
+            workExperiencesData.map((workExperienceDataItem) => {
+              const {
+                role,
+                companyName,
+                companyLogo,
+                duration,
+                description,
+                descriptionBullets,
+              } = workExperienceDataItem;
+              return (
+                <Grid item xs={12} key={Math.random()}>
+                  <InfoCard
+                    title={role}
+                    subtitle={companyName}
+                    metaData={duration}
+                    description={description}
+                    descriptionBullets={descriptionBullets}
+                    imageURL={companyLogo}
+                  />
+                </Grid>
+              );
+            })}
+        </Grid>
+      </AccordionDetails>
+    </Accordion>
   </Grid>
 );
 
