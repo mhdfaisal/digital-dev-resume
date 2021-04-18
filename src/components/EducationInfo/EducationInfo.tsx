cda7e7eb7996b98466eb3abd59e287037b0c4f12
mhdfaisal <fmohd195@gmail.com>
2021-04-18 19:52:49 +0530

    add education info component

--- src/components/EducationInfo/EducationInfo.tsx
+++ src/components/EducationInfo/EducationInfo.tsx
@@ -3,15 +3,37 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import InfoCard from '../shared/InfoCard/InfoCard';
 
+import { educationInfo as educationInfoData } from '../../assets/portfolio-info';
+
 /**
  * A presentational component to display educational information.
  */
 const EducationInfo: React.FC = () => (
-  <Grid>
-    <Typography variant="h6" gutterBottom>
+  <Grid classes={{ root: 'education__info' }}>
+    <Typography variant="h6" gutterBottom color="textSecondary">
       Educational Background
     </Typography>
-    <InfoCard />
+    {educationInfoData &&
+      educationInfoData.map((educationItem) => {
+        const {
+          collegeName,
+          collegeLogo,
+          duration,
+          description,
+          descriptionBullets,
+        } = educationItem;
+        return (
+          <Grid item xs={12} key={Math.random()}>
+            <InfoCard
+              title={collegeName}
+              metaData={duration}
+              description={description}
+              descriptionBullets={descriptionBullets}
+              imageURL={collegeLogo}
+            />
+          </Grid>
+        );
+      })}
   </Grid>
 );
 
