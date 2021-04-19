a83c5360da61407cabeacbce105c6568ade46c02
mhdfaisal <fmohd195@gmail.com>
2021-04-19 21:46:37 +0530

    add github user data to display RecentActivities

--- src/components/RecentActivites/RecentActivities.tsx
+++ src/components/RecentActivites/RecentActivities.tsx
@@ -1,37 +1,80 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import emoji from 'react-easy-emoji';
 
 import RepoCard from '../shared/RepoCard/RepoCard';
+import GithubProfileContext, {
+  RefNode as RecentRepositoriesNodeType,
+} from '../../context/GithubProfileContext';
 
 import './RecentActivities.css';
 
 /**
  * A container component to display recently accessed repositories on github by the user.
  */
-const RecentActivities: React.FC = () => (
-  <Grid container classes={{ root: 'featured__repos' }}>
-    <Grid item md={1} />
-    <Grid item md={10}>
-      <Typography variant="h4" gutterBottom>
-        Recent Activities
-      </Typography>
-      <Paper classes={{ root: 'featured__repos-paper-container' }}>
-        <Grid container spacing={3}>
-          <Grid item sm={4}>
-            <RepoCard />
-          </Grid>
-          <Grid item sm={4}>
-            <RepoCard />
-          </Grid>
-          <Grid item sm={4}>
-            <RepoCard />
-          </Grid>
-        </Grid>
-      </Paper>
+const RecentActivities: React.FC = () => {
+  const { isLoading, error, user } = useContext(GithubProfileContext);
+  const repositories = user?.repositories ?? {};
+  const { nodes = [] } = repositories;
+  // state variable and method for controlled accordion
+  const [expanded, setExpanded] = React.useState<string | false>('panel1');
+
+  // handler for accordion expand state change
+  const handleChange = (panel: string) => (
+    event: React.ChangeEvent<{}>,
+    newExpanded: boolean
+  ) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
+  return (
+    <Grid container classes={{ root: 'recent__activities' }}>
+      <Grid item md={1} />
+      <Grid item md={10}>
+        <Accordion
+          expanded={expanded === 'panel1'}
+          onChange={handleChange('panel1')}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography
+              variant="h6"
+              classes={{ root: 'recent__activities-accordion-title' }}
+            >
+              {emoji('🕰️')} Recently Active Repositories
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Grid
+              container
+              classes={{ root: 'recent__activities-grid-container' }}
+            >
+              <Grid container spacing={3}>
+                {nodes &&
+                  nodes.map((repository) => (
+                    <Grid item sm={4} key={Math.random()}>
+                      <RepoCard
+                        repoDetails={{
+                          ...(repository as RecentRepositoriesNodeType),
+                        }}
+                      />
+                    </Grid>
+                  ))}
+              </Grid>
+            </Grid>
+          </AccordionDetails>
+        </Accordion>
+      </Grid>
     </Grid>
-  </Grid>
-);
+  );
+};
 
 export default RecentActivities;
