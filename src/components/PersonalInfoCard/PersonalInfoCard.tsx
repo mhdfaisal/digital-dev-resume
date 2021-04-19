0b38686fb9d0935c803d4a01423985ec86f23c3c
mhdfaisal <fmohd195@gmail.com>
2021-04-19 15:30:19 +0530

    add github profile name and avatar to person info card.

--- src/components/PersonalInfoCard/PersonalInfoCard.tsx
+++ src/components/PersonalInfoCard/PersonalInfoCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -11,50 +11,57 @@ import ImageAvatar from '../shared/ImageAvatar/ImageAvatar';
 import GithubVCard from '../GithubVCard/GithubVCard';
 
 import { skillTags } from '../../assets/portfolio-info';
+import GithubProfileContext from '../../context/GithubProfileContext';
+
 import './PersonalInfoCard.css';
 
 /**
  * Card to display profile Information
  */
-const PersonalInfoCard: React.FC = () => (
-  <Card classes={{ root: 'person-info__card' }}>
-    <CardContent>
-      <Grid>
-        <Grid
-          item
-          xs={12}
-          classes={{ root: 'personal-info__card-header-grid' }}
-        >
-          {skillTags &&
-            skillTags.map((skillTag) => (
-              <SkillTag key={skillTag?.id} {...skillTag} />
-            ))}
+const PersonalInfoCard = () => {
+  const { isLoading, error, user } = useContext(GithubProfileContext);
+  const name = user?.name ?? '';
+  const avatarUrl = user?.avatarUrl ?? '';
+  return (
+    <Card classes={{ root: 'person-info__card' }}>
+      <CardContent>
+        <Grid>
+          <Grid
+            item
+            xs={12}
+            classes={{ root: 'personal-info__card-header-grid' }}
+          >
+            {skillTags &&
+              skillTags.map((skillTag) => (
+                <SkillTag key={skillTag?.id} {...skillTag} />
+              ))}
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            classes={{ root: 'personal-info__card-avatar-grid' }}
+          >
+            <ImageAvatar src={avatarUrl} alt="github-profile-avatar" />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h6" align="center">
+              {name}
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            classes={{ root: 'personal-info__card-github-grid' }}
+          >
+            <GithubVCard />
+          </Grid>
         </Grid>
-        <Grid
-          item
-          xs={12}
-          classes={{ root: 'personal-info__card-avatar-grid' }}
-        >
-          <ImageAvatar />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h6" align="center">
-            Mohammad Faisal
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          classes={{ root: 'personal-info__card-github-grid' }}
-        >
-          <GithubVCard />
-        </Grid>
-      </Grid>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </Card>
-);
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default PersonalInfoCard;
