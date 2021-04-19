b8a5fc5b6f9f1ead4c0fe4628a62e7330a5e56ae
mhdfaisal <fmohd195@gmail.com>
2021-04-19 21:09:31 +0530

    enclose pinned repos section in an accordion

--- src/components/PinnedRepos/PinnedRepos.tsx
+++ src/components/PinnedRepos/PinnedRepos.tsx
@@ -1,7 +1,11 @@
 import React, { useContext } from 'react';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import emoji from 'react-easy-emoji';
 
 import RepoCard from '../shared/RepoCard/RepoCard';
 import GithubProfileContext, {
@@ -17,27 +21,51 @@ const PinnedRepos: React.FC = () => {
   const { isLoading, error, user } = useContext(GithubProfileContext);
   const pinnedItems = user?.pinnedItems ?? {};
   const { edges = [], totalCount } = pinnedItems;
+  // state variable and method for controlled accordion
+  const [expanded, setExpanded] = React.useState<string | false>('panel1');
+
+  const handleChange = (panel: string) => (
+    event: React.ChangeEvent<{}>,
+    newExpanded: boolean
+  ) => {
+    setExpanded(newExpanded ? panel : false);
+  };
 
   return (
     <Grid container classes={{ root: 'pinned__repos' }}>
       <Grid item md={1} />
       <Grid item md={10}>
-        <Typography variant="h4" gutterBottom>
-          Pinned Repositories
-          {/* <Typography variant="h6">(Total : {totalCount} on Github)</Typography> */}
-        </Typography>
-        <Paper classes={{ root: 'pinned__repos-paper-container' }}>
-          <Grid container spacing={3}>
-            {edges &&
-              edges.map((edge) => (
-                <Grid item sm={4} key={Math.random()}>
-                  <RepoCard
-                    repoDetails={{ ...(edge?.node as PinnedItemsNodeType) }}
-                  />
-                </Grid>
-              ))}
-          </Grid>
-        </Paper>
+        <Accordion
+          expanded={expanded === 'panel1'}
+          onChange={handleChange('panel1')}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography
+              variant="h5"
+              classes={{ root: 'pinned__repos-accordion-title' }}
+            >
+              {emoji('📌')} Pinned Repositories
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Grid container classes={{ root: 'pinned__repos-grid-container' }}>
+              <Grid container spacing={3}>
+                {edges &&
+                  edges.map((edge) => (
+                    <Grid item sm={4} key={Math.random()}>
+                      <RepoCard
+                        repoDetails={{ ...(edge?.node as PinnedItemsNodeType) }}
+                      />
+                    </Grid>
+                  ))}
+              </Grid>
+            </Grid>
+          </AccordionDetails>
+        </Accordion>
       </Grid>
     </Grid>
   );
