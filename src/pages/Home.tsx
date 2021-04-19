fff31640f8061bc7caa6b558ee6a91077453d1cf
mhdfaisal <fmohd195@gmail.com>
2021-04-19 13:09:02 +0530

    fetch user data from github-profile.json file on mount of Home component.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 
 import Header from '../components/Header/Header';
 import Profile from '../components/Profile/Profile';
@@ -27,6 +28,55 @@ const useStyles = makeStyles(() => ({
  */
 const Home: React.FC = () => {
   const classes = useStyles();
+  // state variable and method to hold and set github user profile data.
+  //! Todo replace useState with useReducer implementation for a cleaner approach
+  const [gitHubProfileData, setGithubProfileData] = useState(() => ({
+    isLoading: false,
+    error: '',
+    user: {},
+  }));
+
+  /**
+   * Fetches the github profile data from github-profile.json in /public directory
+   * using axios client and set it inside the component state.
+   *
+   */
+  const getGithubProfileData = async () => {
+    try {
+      setGithubProfileData((prevGithubProfileData) => ({
+        ...prevGithubProfileData,
+        isLoading: true,
+        error: '',
+        user: {},
+      }));
+      const result = await axios.get('/github-profile.json');
+      const user = result?.data?.data?.user ?? {};
+      // set the user data inside state
+      setGithubProfileData((prevGithubProfileData) => ({
+        ...prevGithubProfileData,
+        isLoading: false,
+        error: '',
+        user: { ...user },
+      }));
+    } catch (err) {
+      console.log(err);
+      // set error message inside the state
+      setGithubProfileData((prevGithubProfileData) => ({
+        ...prevGithubProfileData,
+        isLoading: false,
+        error: err?.message ?? '',
+        user: {},
+      }));
+    }
+  };
+
+  /**
+   * effect to call a function to fetch user github profile on mount.
+   */
+  useEffect(() => {
+    getGithubProfileData();
+  }, []);
+
   return (
     <Paper elevation={0} className={classes.root}>
       <Header />
