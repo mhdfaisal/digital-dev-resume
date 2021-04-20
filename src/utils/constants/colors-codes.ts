7bdc193b32c8d0f89266e27efb0a45b8ef5f2179
mhdfaisal <fmohd195@gmail.com>
2021-04-20 13:45:15 +0530

    add theme-modes and color-codes constants

---
+++ src/utils/constants/colors-codes.ts
@@ -0,0 +1,13 @@
+const PALETTE_PRIMARY = {
+  light: '#0a558d',
+  dark: '#000',
+};
+
+const HOME_CONTAINER = {
+  background: {
+    light: '#f5f5f5',
+    dark: '#000',
+  },
+};
+
+export { PALETTE_PRIMARY, HOME_CONTAINER };
---
+++ src/utils/constants/theme-modes.ts
@@ -0,0 +1,4 @@
+export const THEME_MODES = {
+  dark: 'dark',
+  light: 'light',
+};
