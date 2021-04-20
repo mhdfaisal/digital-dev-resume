87fe75c3aa1e66b0c2cf2a17c313b3ae1e83a30f
mhdfaisal <fmohd195@gmail.com>
2021-04-21 00:34:16 +0530

    remove router from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import App from './App';
 
 import './index.css';
 
-ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
-    <Route component={App} path="/" exact />
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
