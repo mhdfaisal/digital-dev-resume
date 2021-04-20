8c8a8b2e77014a6ca465b08badb10a4ffe72a247
mhdfaisal <fmohd195@gmail.com>
2021-04-21 00:19:23 +0530

    add react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+ReactDOM.render(
+  <Router basename={process.env.PUBLIC_URL}>
+    <Route component={App} path="/" exact />
+  </Router>,
+  document.getElementById('root')
+);
