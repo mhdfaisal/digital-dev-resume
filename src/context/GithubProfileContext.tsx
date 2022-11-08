301afd1a64944e8e6b4929d42cbb51f3280eeb99
mhdfaisal <fmohd195@gmail.com>
2022-11-08 16:27:07 +0530

    Remove undefined as type definition from interface and use partial in props in GithubProfileContext.

--- src/context/GithubProfileContext.tsx
+++ src/context/GithubProfileContext.tsx
@@ -2,88 +2,88 @@ import React from 'react';
 
 // type for repositories commits ref node object
 export type RefNode = {
-  name?: 'string';
-  target?: {
-    history?: {
-      totalCount?: number;
+  name: 'string';
+  target: {
+    history: {
+      totalCount: number;
     };
-    messageHeadline?: string;
-    pushedDate?: string;
+    messageHeadline: string;
+    pushedDate: string;
   };
 };
 
 // type for repository topics node.
 export type RepositoryTopicsNode = {
-  topic?: {
-    name?: string;
+  topic: {
+    name: string;
   };
 };
 
 // type for pinned item edge object.
 export type NodeType = {
-  description?: string;
-  diskUsage?: number;
-  forkCount?: number;
-  homepageUrl?: string;
-  id?: string;
-  name?: string;
-  openGraphImageUrl?: string;
-  primaryLanguage?: {
-    color?: string;
-    name?: string;
+  description: string;
+  diskUsage: number;
+  forkCount: number;
+  homepageUrl: string;
+  id: string;
+  name: string;
+  openGraphImageUrl: string;
+  primaryLanguage: {
+    color: string;
+    name: string;
   };
-  pushedAt?: string;
-  refs?: {
-    nodes?: RefNode[];
+  pushedAt: string;
+  refs: {
+    nodes: Partial<RefNode>[];
   };
-  repositoryTopics?: {
-    nodes: RepositoryTopicsNode[];
+  repositoryTopics: {
+    nodes: Partial<RepositoryTopicsNode>[];
   };
-  stargazers?: {
-    totalCount?: number;
+  stargazers: {
+    totalCount: number;
   };
-  url?: string;
-  useCustomOpenGraphImage?: boolean;
+  url: string;
+  useCustomOpenGraphImage: boolean;
 };
 
 // type for an array of pinned item edges.
 export type NodeArr = {
-  node: NodeType[];
+  node: Partial<NodeType>[];
 };
 
 // type for github user object
 export type GithubUser = {
-  avatarUrl?: string;
-  bio?: string;
-  company?: string;
-  isHireable?: string;
-  location?: string;
-  name?: string;
-  status?: {
-    emojiHTML?: string;
-    message?: string;
+  avatarUrl: string;
+  bio: string;
+  company: string;
+  isHireable: string;
+  location: string;
+  name: string;
+  status: {
+    emojiHTML: string;
+    message: string;
   };
-  url?: string;
-  pinnedItems?: {
-    totalCount?: number;
-    edges?: NodeArr[];
+  url: string;
+  pinnedItems: {
+    totalCount: number;
+    edges: NodeArr[];
   };
-  repositories?: {
-    nodes?: NodeType[];
+  repositories: {
+    nodes: Partial<NodeType>[];
   };
 };
 
 export interface IGithubProfileContext {
   isLoading: boolean;
   error: string;
-  user?: GithubUser;
+  user: Partial<GithubUser>;
 }
 
 // default value for user object
-const providerUserDefaultValue: GithubUser = {};
+const providerUserDefaultValue: Partial<GithubUser> = {};
 
 // default value for github profile context provider
-const providerDefaultValue: IGithubProfileContext = {
+const providerDefaultValue: Partial<IGithubProfileContext> = {
   isLoading: false,
   error: '',
   user: providerUserDefaultValue,
@@ -93,8 +93,7 @@ const providerDefaultValue: IGithubProfileContext = {
  * Context to provide github user data to the components
  * enclosed within it's provider and avoid props drilling.
  */
-const GithubProfileContext: React.Context<IGithubProfileContext> = React.createContext(
-  providerDefaultValue
-);
+const GithubProfileContext: React.Context<Partial<IGithubProfileContext>> =
+  React.createContext(providerDefaultValue);
 
 export default GithubProfileContext;
